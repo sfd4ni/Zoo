c15a59481b7195ae551ee34bf38eef6eaa3ae0f4
Daniel Barroso Rocío <danielbarrosorocio@gmail.com>
2021-11-30 12:56:56 +0000

    Animal delete and create

--- app/src/main/java/es/system/daniel/zoo/view/animals/DeleteAnimalActivity.java
+++ app/src/main/java/es/system/daniel/zoo/view/animals/DeleteAnimalActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.text.ParseException;
 
 import es.system.daniel.R;
+import es.system.daniel.zoo.dao.helpers.AnimalDbHelper;
 
 public class DeleteAnimalActivity extends AppCompatActivity {
 
+    public AnimalDbHelper animalDbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +24,23 @@ public class DeleteAnimalActivity extends AppCompatActivity {
         Intent previousView = new Intent(DeleteAnimalActivity.this,
                 AnimalsActivity.class);
         startActivity(previousView);
+    }
+    public void deleteAnimal(View view) {
+        animalDbHelper = new AnimalDbHelper(this);
+        EditText editTextName = (EditText) this.findViewById(R.id.editTextId);
+
+        String idStr = editTextName.getText().toString();
+
+        if (!idStr.matches("")) {
+            Integer id = null;
+            try {
+                id = Integer.parseInt(idStr);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (id != null) {
+                animalDbHelper.delete(idStr);
+            }
+        }
     }
 }
