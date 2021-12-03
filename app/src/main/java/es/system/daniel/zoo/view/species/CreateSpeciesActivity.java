052ecbbd2fc838f8965e885c46f2dcd9aa744d62
Daniel Barroso Rocío <danielbarrosorocio@gmail.com>
2021-12-03 14:34:22 +0000

    Creado spinner desde resource strings

--- app/src/main/java/es/system/daniel/zoo/view/species/CreateSpeciesActivity.java
+++ app/src/main/java/es/system/daniel/zoo/view/species/CreateSpeciesActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import es.system.daniel.R;
 import es.system.daniel.zoo.dao.helpers.SpeciesDbHelper;
@@ -23,6 +25,14 @@ public class CreateSpeciesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_species);
         speciesDbHelper = new SpeciesDbHelper(this);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
     public void changeSpeciesView(View view) {
         Intent previousView = new Intent(CreateSpeciesActivity.this,
@@ -30,21 +40,22 @@ public class CreateSpeciesActivity extends AppCompatActivity {
         startActivity(previousView);
     }
     public void insertSpecies(View view) {
-        EditText editTextVulgarName = (EditText) this.findViewById(R.id.editTextId);
-        EditText editTextScientificName = (EditText) this.findViewById(R.id.editTextSex);
-        EditText editTextFamily = (EditText) this.findViewById(R.id.editTextCountry);
-        EditText editTextEndangered = (EditText) this.findViewById(R.id.editTextContinent);
+
+        EditText editTextVulgarName = (EditText) this.findViewById(R.id.editTextVulgarName);
+        EditText editTextScientificName = (EditText) this.findViewById(R.id.editTextScientificName);
+        EditText editTextFamily = (EditText) this.findViewById(R.id.editTextFamily);
+        Spinner spinnerEndangered = (Spinner) this.findViewById(R.id.spinner);
 
         String vulgarName = editTextVulgarName.getText().toString();
         String scientificName = editTextScientificName.getText().toString();
         String family = editTextFamily.getText().toString();
-        String endangered = editTextEndangered.getText().toString();
+        String endangered = spinnerEndangered.getSelectedItem().toString();
 
         if (!vulgarName.equals("")  && !scientificName.equals("")
             && !family.equals("") && !endangered.equals("")) {
             Boolean endangeredBool = false;
             try {
-                endangeredBool = endangered.equals("si");
+                endangeredBool = endangered.equals("Yes");
             } catch (Exception e) {
                 e.printStackTrace();
             }
