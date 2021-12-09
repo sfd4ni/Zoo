fe95cdffc9f04dfbc6d79d4e4c8296976ef54762
Daniel Barroso Rocío <danielbarrosorocio@gmail.com>
2021-12-09 13:31:54 +0000

    Arreglada vista y añadido caracteristicas

--- app/src/main/java/es/system/daniel/zoo/view/species/DeleteSpeciesActivity.java
+++ app/src/main/java/es/system/daniel/zoo/view/species/DeleteSpeciesActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import es.system.daniel.R;
+import es.system.daniel.zoo.dao.helpers.SpeciesDbHelper;
 import es.system.daniel.zoo.view.animals.AnimalsActivity;
 import es.system.daniel.zoo.view.animals.DeleteAnimalActivity;
 
 public class DeleteSpeciesActivity extends AppCompatActivity {
 
+    SpeciesDbHelper speciesDbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +24,15 @@ public class DeleteSpeciesActivity extends AppCompatActivity {
         Intent previousView = new Intent(DeleteSpeciesActivity.this,
                 SpeciesActivity.class);
         startActivity(previousView);
+    }
+    public void deleteSpecies(View view) {
+        speciesDbHelper = new SpeciesDbHelper(this);
+        EditText editTextVulgarName = (EditText) this.findViewById(R.id.editTextDeleteSpecies);
+
+        String idStr = editTextVulgarName.getText().toString();
+
+        if (!idStr.matches("")) {
+            speciesDbHelper.delete(idStr);
+        }
     }
 }
--- app/src/main/java/es/system/daniel/zoo/view/species/UpdateSpeciesActivity.java
+++ app/src/main/java/es/system/daniel/zoo/view/species/UpdateSpeciesActivity.java
@@ -5,19 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import es.system.daniel.R;
+import es.system.daniel.zoo.dao.helpers.SpeciesDbHelper;
+import es.system.daniel.zoo.model.Species;
 
 public class UpdateSpeciesActivity extends AppCompatActivity {
+
+    public SpeciesDbHelper speciesDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_species);
+        speciesDbHelper = new SpeciesDbHelper(this);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
     public void changeSpeciesView(View view) {
         Intent previousView = new Intent(UpdateSpeciesActivity.this,
                 SpeciesActivity.class);
         startActivity(previousView);
+    }
+    public void updateSpecies(View view) {
+
+        EditText editTextVulgarName = (EditText) this.findViewById(R.id.editTextVulgarName);
+        EditText editTextScientificName = (EditText) this.findViewById(R.id.editTextScientificName);
+        EditText editTextFamily = (EditText) this.findViewById(R.id.editTextFamily);
+        Spinner spinnerEndangered = (Spinner) this.findViewById(R.id.spinner);
+
+        String vulgarName = editTextVulgarName.getText().toString();
+        String scientificName = editTextScientificName.getText().toString();
+        String family = editTextFamily.getText().toString();
+        String endangered = spinnerEndangered.getSelectedItem().toString();
+
+        if (!vulgarName.equals("")  && !scientificName.equals("")
+                && !family.equals("") && !endangered.equals("")) {
+            Boolean endangeredBool = false;
+            try {
+                endangeredBool = endangered.equals("Yes");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Species species = new Species(vulgarName, scientificName, family, endangeredBool);
+            speciesDbHelper.update(species, vulgarName);
+        }
     }
 }
